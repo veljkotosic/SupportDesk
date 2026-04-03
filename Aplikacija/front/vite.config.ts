6f8a64da317f6fd76196e4d946b808ec565451b3
Veljko Tosic <veljkotosic6@gmail.com>
2026-04-03 17:34:28 +0200

    Init

---
+++ Aplikacija/front/vite.config.ts
@@ -0,0 +1,44 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+
+  server: {
+    host: true,
+    strictPort: false,
+    allowedHosts: ['.ngrok-free.app'],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5266',
+        changeOrigin: true
+      },
+      '/hubs': {
+        target: 'http://localhost:5039',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'if-function'],
+      },
+    },
+  },
+})
